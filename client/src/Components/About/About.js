bca3b5753224e077605690e2d61065b59882bac2
Kunal0220 <ksrivastava0220@gmail.com>
2023-04-23 14:42:40 +0530

    All set , just generator remaining

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -10,6 +10,27 @@ function About() {
 
         <img src={require('../imgs/about.png')} className='aboutImg' alt="" />
 
+      <p className='WRW'> <span> Who are we ? </span><br />
+      We are a group of people who want to make apps <br />
+      so that people don't get stuck in mere work that <br />
+      actually is of great importance!
+      </p>
+      
+      <p className="WRWH"> <span> Why are we here ?</span><br />
+      We are here to deliver a product which will not only <br />
+      save your time but will also give you the best experience <br />
+      while creating a ReadMe . <br /> 
+      PS: We have used the same app (GeneratorEagle) for this project's Readme 🤐.
+      </p>
+
+      <p className="WRWG"><span>Where are we Going?</span> <br />
+      We are Going to take this project to a next level by <br />
+      integrating GitHub directly into it so that if you learn <br />
+      any new skill or earn any achievement then it automatically <br />
+      gets integrated to your ReadMe. 
+      <br /><br />
+      </p>
+
       </div>
       <br /><br />
       <br /><br />
--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -5,6 +5,10 @@ function Landing() {
     <div className='wave'>
     <img className='bannerImg' data-aos="slide-right" src={require('../imgs/landing.png')} alt="" />
         
+
+      <div className="landingMainContainer">
+
+
         <div className="wave2">
 
          <h1 className="LandingBannerText " >Generator Eagle Presents</h1>
@@ -12,9 +16,20 @@ function Landing() {
          <h4 className='LandingPageSecondText'> An Awesome Place to solve all your ReadMe problems</h4>
         </div>
 
+        <div className="container landingMainText mt-4">
+          <p>
+        Create an impressive and professional GitHub profile with ease using Eagle Generator. Stand out in the competitive world of software development and showcase your skills, projects, and contributions in a visually appealing way. <br /><br />
+         Whether you're a seasoned developer or just starting your coding journey, our app makes it easy for you to create a standout GitHub profile that leaves a lasting impression on potential employers, collaborators, and open-source communities. <br /><br />
+        Don't let your GitHub profile be an afterthought. Make it a powerful tool to showcase your skills and expertise with Eagle Generator. Create a professional and impressive GitHub profile that sets you apart from the competition. Get started today and take your software development career to new heights!
+          </p>
+
+          <button className="btn btn-dark">Generate Now</button>
+        </div>
+
+      </div>
 
 
-    </div>
+    </div>      
   )
 }
 
